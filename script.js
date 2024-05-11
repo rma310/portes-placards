063752be9646136ac563ba9c447100a29e959b61
rma310 <francoisflahaut@gmail.com>
2024-05-11 17:40:05 +0200

    load fonts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let globalScrollPosition = 0;
 
 window.onscroll = function (e) {
-    const header = document.getElementById("main-header");
+    const header = document.getElementById("header");
     const scrollPosition = window.scrollY;
 
     if (scrollPosition >= globalScrollPosition) {
